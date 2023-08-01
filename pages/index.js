d6c520730f8e3d478605042f9bc5cf1c50eb76f5
Dilang <dilang@berkeley.edu>
2023-08-01 15:26:07 -0700

    added a back button on the result page

--- pages/index.js
+++ pages/index.js
@@ -2,28 +2,28 @@ import { useState } from 'react';
 import { useRouter } from 'next/router';
 
 export default function Home() {
-  const [number, setNumber] = useState('');
-  const router = useRouter();
+    const [number, setNumber] = useState('');
+    const router = useRouter();
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    router.push(`/result/${number}`);
-  };
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        router.push(`/result/${number}`);
+    };
 
-  return (
-    <div>
-      <h1>Fibonacci Generator</h1>
-      <h3>Enter a positive integer to generate the first nth Fibonacci numbers:</h3>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="number"
-          min="1"
-          value={number}
-          onChange={(e) => setNumber(e.target.value)}
-          required
-        />
-        <button type="submit">Generate</button>
-      </form>
-    </div>
-  );
+    return (
+        <div>
+        <h1>Fibonacci Generator</h1>
+        <h3>Enter a positive integer to generate the first nth Fibonacci numbers:</h3>
+        <form onSubmit={handleSubmit}>
+            <input
+            type="number"
+            min="1"
+            value={number}
+            onChange={(e) => setNumber(e.target.value)}
+            required
+            />
+            <button type="submit">Generate</button>
+        </form>
+        </div>
+    );
 }
--- pages/result/[number].js
+++ pages/result/[number].js
@@ -2,22 +2,28 @@ import { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 
 export default function Result() {
-  const router = useRouter();
-  const { number } = router.query;
-  const [result, setResult] = useState([]);
+    const router = useRouter();
+    const { number } = router.query;
+    const [result, setResult] = useState([]);
 
-  useEffect(() => {
-    if (number) {
-      fetch(`/api/fibonacci?number=${number}`)
-        .then((response) => response.json())
-        .then((data) => setResult(data.result));
-    }
-  }, [number]);
+    useEffect(() => {
+        if (number) {
+        fetch(`/api/fibonacci?number=${number}`)
+            .then((response) => response.json())
+            .then((data) => setResult(data.result));
+        }
+    }, [number]);
 
-  return (
-    <div>
-      <h1>Fibonacci Sequence</h1>
-      <p>{result.join(', ')}</p>
-    </div>
-  );
+    function goBack() {
+        router.push('/')
+    };
+
+    return (
+        <div>
+        <h1>Fibonacci Sequence</h1>
+        <h3>The first {number} Fibonacci numbers are:</h3>
+        <p>{result.join(', ')}</p>
+        <button onClick={goBack}>Go Back</button>
+        </div>
+    );
 }
